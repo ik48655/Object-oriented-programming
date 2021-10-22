71ba232bf7cee7f3fd6a78461e9ab1792d0bfacd
ik48655 <iv4nchee@gmail.com>
2021-10-22 18:28:00 +0200

    Objektno programiranje
    Vjezbe iz OP

--- Vjezba2/Zadatak2/Zadatak2.cpp
+++ Vjezba2/Zadatak2/Zadatak2.cpp
@@ -8,10 +8,10 @@ void ArrIn(int* arr, int n) // Unosimo niz
 		cin >> arr[i];
 	}
 }
-int& ElemRet(int* arr, int selected) // Vracamo clan
+int& ElemRet(int* arr, int trazeni) // Vracamo clan
 {
 
-	return (arr[selected]);
+	return (arr[trazeni]);
 }
 void ArrOut(int* arr, int n) // Printanje niza
 {
@@ -29,16 +29,16 @@ int main()
 	int* arr = new int[n]; // Alociramo memoriju
 	cout << "Unesite elemente niza: " << endl;
 	ArrIn(arr, n);
-	int selected;
+	int trazeni;
 	cout << endl << "Unesite redni broj elementa kojeg zelite uvecati za 1: ";
-	cin >> selected;
-	while ((selected > n) || (selected < 0)) //Fail-safe tj provjera da li je uneseni broj jedan od indeksa clanova niza 
+	cin >> trazeni;
+	while ((trazeni > n) || (trazeni < 0)) //Fail-safe tj provjera da li je uneseni broj jedan od indeksa clanova niza 
 	{
 		cout << endl << "Unesena vrijednost nije unutar niza!" << endl << "Unesite redni broj elementa kojeg zelite uvecati za 1: ";
-		cin >> selected;
+		cin >> trazeni;
 	}
-	selected--;
-	ElemRet(arr, selected) += 1; // Ovdje vracamo odabran n-ti clan uvecan za jedan
+	trazeni--;
+	ElemRet(arr, trazeni) += 1; // Ovdje vracamo odabran n-ti clan uvecan za jedan
 	ArrOut(arr, n);
 	delete[] arr; // Brisemo memoriju
 	return 0;
